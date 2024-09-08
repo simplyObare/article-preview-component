3394154a16238bb4c5ff67f09d63f1447b6f1b82
simplyObare <josephatowiny@gmail.com>
2024-09-08 21:45:00 +0300

    project complete

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-// Toggle desktop shae button
-const shareButton = document.getElementsByClassName('share-btn-container')
+// Toggle desktop share button
+const desktopShareButton = document.getElementById('desktop-share-btn')
+const shareBubbleContainer = document.getElementsByClassName(
+  'share-bubble-container'
+)
 
+desktopShareButton.addEventListener('click', () => {
+  shareBubbleContainer[0].classList.toggle('display-none')
+})
+
+// mobile share toggle
+
+const mobileAuthorArea = document.querySelector('.mobile-author-area')
+const mobileSocialSharingBar = document.querySelector(
+  '.mobile-social-share-bar'
+)
+const mobileShareButton = document.querySelector('.mobile-share-btn-container')
+
+mobileShareButton.addEventListener('click', () => {
+  mobileAuthorArea.style.display = 'none'
+  mobileSocialSharingBar.style.display = 'flex'
+})
+
+// Mobile social sharing bar
+
+const mobileSocialShareBtn = document.querySelector('.mobile-social-share-btn')
+
+mobileSocialShareBtn.addEventListener('click', () => {
+  mobileAuthorArea.style.display = 'flex'
+  mobileSocialSharingBar.style.display = 'none'
+})
